c82ff15a60c19575fdbac0a5e6bb6c8705cbdfec
VergilSpardov <pilipch04@gmail.com>
2023-10-17 23:49:52 +0300

    HomeWork2

---
+++ Assets/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+
+    public int counter = 0;
+
+    public Text counterText;
+
+    void Start()
+    {
+        UpdateCounterText();
+    }
+
+    void UpdateCounterText()
+    {
+        counterText.text = "Счёт: " + counter.ToString();
+    }
+
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        counter++;
+        UpdateCounterText();
+        print(counter);
+        
+    }
+
+}
